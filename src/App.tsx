b640248da942567c472fc170833b30c15bf984dd
Juan Gómez Carrillo <juan.gomez@interficie.com>
2022-04-16 20:17:44 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>Vite Custom Template</h1>
+      <p>Preconfigured for:</p>
+      <ul>
+        <li>React</li>
+        <li>TypeScript</li>
+        <li>ESLint</li>
+        <li>Prettier</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
